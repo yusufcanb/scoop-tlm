3574e3c964db15fab6f36b1ae28f27ba3f91fcbf
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2024-03-17 00:41:04 +0100

    update

--- scripts/pre_install.ps1
+++
@@ -1,5 +0,0 @@
-# Remove the tlm directory if it exists
-if (Test-Path \"C:\\Users\\$env:USERNAME\\AppData\\Local\\Programs\\tlm\") {
-    Write-Output "Manual installation of tlm detected. Removing tlm directory to switch scoop."
-    Remove-Item -Recurse -Force \"C:\\Users\\$env:USERNAME\\AppData\\Local\\Programs\\tlm\"
-}
